21f882db792b2c8bbd584b9646546708a859a135
Eyal Reuveni <eyal@eventbrite.com>
2015-04-24 13:20:57 -0700

    Update setup.py
    Add back in the non murmur3 hashes from smhasher

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ from setuptools import setup, Extension
 libraries = {
     'fnv': ['hash_32.c', 'hash_32a.c', 'hash_64.c', 'hash_64a.c'],
     #'smhasher': ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp', 'City.cpp', 'Spooky.cpp'],
+    'smhasher': ['City.cpp', 'Spooky.cpp'],
     #'MurmurHash': ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp'],
     #'cityhash': ['src/City.cc'],
     'lookup3': ['lookup3.c'],
